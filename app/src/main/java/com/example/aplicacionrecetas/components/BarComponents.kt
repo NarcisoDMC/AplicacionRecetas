343581e17b8f895166f09b8dde0cce0e9f625ca8
NarcisoDMC <davidmadefukingtroll810@gmail.com>
2025-02-28 21:04:33 -0600

    Firts changes to the app

---
+++ app/src/main/java/com/example/aplicacionrecetas/components/BarComponents.kt
@@ -0,0 +1,11 @@
+package com.example.aplicacionrecetas.components
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun TitleBar(name: String){
+    Text(text = name, fontSize = 25.sp, color = Color.White)
+}
